05b50109517760527c785c490fcba9aaea6b34bd
Alberto Leal <mail4alberto@gmail.com>
2018-12-09 03:27:24 -0500

    Complete day 1 puzzle

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,4 +1,25 @@
+// https://adventofcode.com/2018/day/1
+
 fn main() {
-    let my_str = include_str!("input.txt");
-    println!("Day 1: {}", my_str);
+    let input_string = include_str!("input.txt");
+
+    let mut inputs: Vec<&str> = input_string.split('\n').collect();
+
+    // according to the puzzle, the starting frequency is 0
+    let mut current_frequency = 0;
+
+    while let Some(raw_input) = inputs.pop() {
+        let input = raw_input.trim();
+
+        if input.is_empty() {
+            // skip empty input
+            continue;
+        }
+
+        let parsed_frequency: i32 = input.parse().unwrap();
+
+        current_frequency += parsed_frequency;
+    }
+
+    println!("Resulting frequency: {}", current_frequency);
 }
