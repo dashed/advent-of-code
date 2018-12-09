9499a92242e29832f36fd9de16b92e70829064cc
Alberto Leal <mail4alberto@gmail.com>
2018-12-09 17:42:11 -0500

    init day 3

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,3 +1,21 @@
+// https://adventofcode.com/2018/day/3
+
+// imports
+
+use std::str::Lines;
+
+// part 1
+
+fn part_1(inputs: Lines) {
+    for input in inputs {
+        println!("{}", input);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_string = include_str!("input.txt");
+
+    let inputs = input_string.lines();
+
+    part_1(inputs);
 }
