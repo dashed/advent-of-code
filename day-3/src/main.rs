bef80f0a6c26e49385a53abb712ae26fcba980ec
Alberto Leal <mail4alberto@gmail.com>
2018-12-09 17:48:19 -0500

    Fabric types and parse_to_fabric

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -6,8 +6,33 @@ use std::str::Lines;
 
 // part 1
 
+struct Fabric {
+    id: String,
+
+    // starting coordinates
+    left: i32,
+    top: i32,
+
+    // size
+    height: i32,
+    width: i32,
+}
+
+fn parse_to_fabric(input: &str) -> Fabric {
+    // TODO: parsing magic
+
+    Fabric {
+        id: "123".to_string(),
+        left: 123,
+        top: 123,
+        height: 123,
+        width: 123,
+    }
+}
+
 fn part_1(inputs: Lines) {
     for input in inputs {
+        let fabric = parse_to_fabric(input);
         println!("{}", input);
     }
 }
