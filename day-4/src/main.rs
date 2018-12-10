aadf3aff33f9e6b6c129bf4ec7244063fabeb421
Alberto Leal <mail4alberto@gmail.com>
2018-12-10 02:00:44 -0500

    start day 4

--- day-4/src/main.rs
+++ day-4/src/main.rs
@@ -1,3 +1,27 @@
+// https://adventofcode.com/2018/day/4
+
+// imports
+
+use std::collections::HashMap;
+
+// types
+
+type Minute = i32;
+type Occurences = i32;
+
+struct Guard {
+    minutes_slept: i32,
+
+    // which minute portion on midnight was the guard sleeping at, and how many times?
+    occurences_counter: HashMap<Minute, Occurences>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_string = include_str!("input.txt");
+
+    let inputs = input_string.lines();
+
+    for input in inputs {
+        println!("{:?}", input);
+    }
 }
