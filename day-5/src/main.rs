440e68ee4dc863fb13a64eb7dd98439a5bb83769
Alberto Leal <mail4alberto@gmail.com>
2018-12-10 17:00:15 -0500

    does_react

--- day-5/src/main.rs
+++ day-5/src/main.rs
@@ -8,6 +8,10 @@ fn is_opposite_polarity(x: char, y: char) -> bool {
     return (x.is_uppercase() && y.is_lowercase()) || (y.is_uppercase() && x.is_lowercase());
 }
 
+fn does_react(x: char, y: char) -> bool {
+    return is_same_type(x, y) && is_opposite_polarity(x, y);
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
@@ -31,5 +35,12 @@ mod tests {
         assert_eq!(is_opposite_polarity('B', 'B'), false);
         assert_eq!(is_opposite_polarity('a', 'A'), true);
         assert_eq!(is_opposite_polarity('a', 'B'), true);
+    }
+
+    #[test]
+    fn test_does_react() {
+        assert_eq!(does_react('a', 'a'), false);
+        assert_eq!(does_react('A', 'A'), false);
+        assert_eq!(does_react('a', 'A'), true);
     }
 }
