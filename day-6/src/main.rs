70a85886beab2f015f5c8c23bc66b1061e3acbf8
Alberto Leal <mail4alberto@gmail.com>
2018-12-10 20:36:47 -0500

    utils

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,5 +1,9 @@
 // https://adventofcode.com/2018/day/6
 
+// imports
+
+use std::collections::HashSet;
+
 // helpers
 
 type Position = (i32, i32);
@@ -21,10 +25,50 @@ fn parse_to_coord(input: &str) -> Position {
     (*result.get(0).unwrap(), *result.get(1).unwrap())
 }
 
+fn get_x(src: Position) -> i32 {
+    let (x, _y) = src;
+    return x;
+}
+
+fn get_y(src: Position) -> i32 {
+    let (_x, y) = src;
+    return y;
+}
+
+fn is_better_top_edge(reference: Position, target: Position) -> bool {
+    let ref_point = get_y(reference);
+    let target_point = get_y(target);
+    return target_point > ref_point;
+}
+
+fn is_better_bottom_edge(reference: Position, target: Position) -> bool {
+    let ref_point = get_y(reference);
+    let target_point = get_y(target);
+    return target_point < ref_point;
+}
+
+fn is_better_left_edge(reference: Position, target: Position) -> bool {
+    let ref_point = get_x(reference);
+    let target_point = get_x(target);
+    return target_point < ref_point;
+}
+
+fn is_better_right_edge(reference: Position, target: Position) -> bool {
+    let ref_point = get_x(reference);
+    let target_point = get_x(target);
+    return target_point > ref_point;
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
     let destinations: Vec<Position> = input_string.lines().map(parse_to_coord).collect();
+
+    let ignored_destinations: HashSet<Position> = HashSet::new();
+
+    // filter out destinations on the edge of the bounding box (i.e. grid).
+    // we want to do this because regions for these destinations have infinite area.
+
 
     println!("{:?}", destinations);
 }
