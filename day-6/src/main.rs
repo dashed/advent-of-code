2b98185c0408eff8dd8a5e1a9eae5dd9c09cb0c0
Alberto Leal <mail4alberto@gmail.com>
2018-12-10 20:15:08 -0500

    flesh out

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -1,5 +1,30 @@
+// https://adventofcode.com/2018/day/6
+
+// helpers
+
+type Position = (i32, i32);
+
+// https://math.stackexchange.com/a/139604/10247
+fn get_manhattan_distance(x: Position, y: Position) -> i32 {
+    let (a, b) = x;
+    let (c, d) = y;
+
+    return (a - c).abs() + (b - d).abs();
+}
+
+fn parse_to_coord(input: &str) -> Position {
+    let result: Vec<i32> = input
+        .split(',')
+        .map(|x| -> i32 { x.trim().parse().unwrap() })
+        .collect();
+
+    (*result.get(0).unwrap(), *result.get(1).unwrap())
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
-    println!("{}", input_string);
+    let destinations: Vec<Position> = input_string.lines().map(parse_to_coord).collect();
+
+    println!("{:?}", destinations);
 }
