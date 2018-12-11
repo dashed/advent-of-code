b7bcba64a7d4c37273abd7998cddcd3d95fe3b2f
Alberto Leal <mail4alberto@gmail.com>
2018-12-10 20:49:53 -0500

    bounding box

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -59,18 +59,67 @@ fn is_better_right_edge(reference: Position, target: Position) -> bool {
     return target_point > ref_point;
 }
 
+#[derive(Debug, Clone)]
+struct BoundingBox {
+    top: Position,
+    bottom: Position,
+    left: Position,
+    right: Position,
+}
+
+impl BoundingBox {
+    fn new(start: Position) -> BoundingBox {
+        BoundingBox {
+            top: start,
+            bottom: start,
+            left: start,
+            right: start,
+        }
+    }
+
+    fn add_point(&self, src: Position) -> BoundingBox {
+        let mut cloned = self.clone();
+
+        if is_better_top_edge(self.top, src) {
+            cloned.top = src;
+        }
+
+        if is_better_bottom_edge(self.bottom, src) {
+            cloned.bottom = src;
+        }
+
+        if is_better_left_edge(self.left, src) {
+            cloned.left = src;
+        }
+
+        if is_better_right_edge(self.right, src) {
+            cloned.right = src;
+        }
+
+        return cloned;
+    }
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
     let destinations: Vec<Position> = input_string.lines().map(parse_to_coord).collect();
 
-    let ignored_destinations: HashSet<Position> = HashSet::new();
+    // let ignored_destinations: HashSet<Position> = HashSet::new();
 
     // filter out destinations on the edge of the bounding box (i.e. grid).
     // we want to do this because regions for these destinations have infinite area.
 
+    let bounding_box = destinations
+        .iter()
+        .fold(None, |acc: Option<BoundingBox>, dest| match acc {
+            None => return Some(BoundingBox::new(*dest)),
+            Some(bounding_box) => {
+                return Some(bounding_box.add_point(*dest));
+            }
+        });
 
-    println!("{:?}", destinations);
+    println!("{:?}", bounding_box);
 }
 
 #[cfg(test)]
