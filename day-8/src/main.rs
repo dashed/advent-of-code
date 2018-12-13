3c13c324a82b40d26fff907c2a568c6596e37050
Alberto Leal <mail4alberto@gmail.com>
2018-12-12 19:04:03 -0500

    parse input

--- day-8/src/main.rs
+++ day-8/src/main.rs
@@ -1,3 +1,22 @@
+// https://adventofcode.com/2018/day/8
+
+struct Node {
+    num_of_child_nodes: i32,
+    num_of_meta_entries: i32,
+
+    children: Vec<Node>,
+
+    metadata: Vec<i32>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_string = include_str!("input.txt");
+
+    let iter = input_string.trim().split_whitespace().map(|x| -> i32 {
+        return x.trim().parse().unwrap();
+    });
+
+    for input in iter {
+        println!("{:?}", input);
+    }
 }
