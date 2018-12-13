aeb55a87164a2cd445c595ea2e9303b9cf8b1680
Alberto Leal <mail4alberto@gmail.com>
2018-12-13 05:39:15 -0500

    get_total_power_level_of_square

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -24,6 +24,30 @@ fn get_power_level(x: i32, y: i32, grid_serial_number: i32) -> i32 {
     return power_level - 5;
 }
 
+fn get_total_power_level_of_square(
+    start_x: i32,
+    start_y: i32,
+    size: i32,
+    grid_serial_number: i32,
+) -> i32 {
+    let size = size - 1;
+
+    // end_y - start_x + 1 = size
+    let end_x = size + start_x - 1;
+    // similarly for end_y
+    let end_y = size + start_y - 1;
+
+    let mut total = 0;
+
+    for x in start_x..=end_x {
+        for y in start_y..=end_y {
+            total += get_power_level(x, y, grid_serial_number);
+        }
+    }
+
+    return total;
+}
+
 fn main() {
     let grid_serial_number = 4172;
 
@@ -44,7 +68,7 @@ mod tests {
         assert_eq!(get_power_level(3, 5, 8), 4);
         assert_eq!(get_power_level(122, 79, 57), -5);
         assert_eq!(get_power_level(217, 196, 39), 0);
-        assert_eq!(get_power_level(101,153, 71), 4);
+        assert_eq!(get_power_level(101, 153, 71), 4);
     }
 
 }
