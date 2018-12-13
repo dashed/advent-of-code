0887c5bcdf78d2b4a94215763d797af5cb46c145
Alberto Leal <mail4alberto@gmail.com>
2018-12-13 03:46:49 -0500

    parse_input_to_star

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,24 +1,73 @@
+// https://adventofcode.com/2018/day/10
+
+// imports
+
 extern crate image;
+
+// helpers
+
+fn substring(this: &str, start: usize, len: usize) -> String {
+    this.chars().skip(start).take(len).collect()
+}
 
 struct Star {
     position: (i32, i32),
     velocity: (i32, i32),
 }
 
-fn parse_input_to_star(input: &str) {
+fn parse_input_to_star(input: &str) -> Star {
 
+    let input: Vec<&str> = input.trim().split("velocity").map(|x| x.trim()).collect();
+
+    let position: (i32, i32) = {
+
+        let raw_string = input.get(0).unwrap();
+
+        let skip = "position=<".len();
+        let end = raw_string.len() - skip - 1;
+        let raw_string = substring(raw_string, skip, end);
+
+        let tokens: Vec<i32> = raw_string.split(',').map(|x| -> i32 {
+            return x.trim().parse().unwrap();
+        }).collect();
+
+        (*tokens.get(0).unwrap(), *tokens.get(1).unwrap())
+    };
+
+
+    let velocity: (i32, i32) = {
+
+        let raw_string = input.get(1).unwrap();
+
+        let skip = "=<".len();
+        let end = raw_string.len() - skip - 1;
+        let raw_string = substring(raw_string, skip, end);
+
+        let tokens: Vec<i32> = raw_string.split(',').map(|x| -> i32 {
+            return x.trim().parse().unwrap();
+        }).collect();
+
+        (*tokens.get(0).unwrap(), *tokens.get(1).unwrap())
+    };
+
+    Star {
+        position,
+        velocity
+    }
 }
 
 fn main() {
 
     let input_string = include_str!("input.txt");
-    // println!("{:?}", input_string);
 
-    let width = 10000;
-    let height = 10000;
+    let inputs: Vec<_> = input_string.trim().lines().map(|x| parse_input_to_star(x)).collect();
 
-    // Create a new ImgBuf with width: imgx and height: imgy
-    // let mut imgbuf = image::GrayImage::new(width, height);
+    println!("{:?}", inputs.len());
+
+    let width = 1000;
+    let height = 1000;
+
+    let mut img_buffer = image::GrayImage::new(width, height);
 
     // Iterate over the coordinates and pixels of the image
     // for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
