913415d14e0651138e37858a0ba6ec08a2b005ff
Alberto Leal <mail4alberto@gmail.com>
2018-12-12 20:59:49 -0500

    game state

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,5 +1,10 @@
 // https://adventofcode.com/2018/day/9
 
 fn main() {
-    println!("Hello, world!");
+    let input_string = include_str!("input.txt");
+
+    // init marble game with the first marble in the circle
+    let game_state: Vec<i32> = vec![0];
+
+    println!("{}", input_string);
 }
