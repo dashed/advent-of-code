a0a9656630db15be7495bcceee69e814d6ea1eed
Alberto Leal <mail4alberto@gmail.com>
2018-12-13 01:44:17 -0500

    image

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -1,3 +1,23 @@
+extern crate image;
+
 fn main() {
-    println!("Hello, world!");
+
+    // let input_string = include_str!("input.txt");
+    // println!("{:?}", input_string);
+
+    let width = 1000;
+    let height = 1000;
+
+    // Create a new ImgBuf with width: imgx and height: imgy
+    let mut imgbuf = image::GrayImage::new(width, height);
+
+    // Iterate over the coordinates and pixels of the image
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+
+        let color: u8 = (x % 256) as u8;
+
+        *pixel = image::Luma([color]);
+    }
+
+    imgbuf.save("fractal.png").unwrap();
 }
