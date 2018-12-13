31d978a45bcf10be5402a9788354008b27d743d1
Alberto Leal <mail4alberto@gmail.com>
2018-12-12 22:25:44 -0500

    flesh out

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -27,6 +27,23 @@ impl GameState {
             players,
         }
     }
+
+    fn get_position_clockwise(&self, pos_to_move: usize) -> usize {
+        // get vector position that is pos_to_move to the right (i.e. clockwise) of pos_of_current_marble
+        return get_position_clockwise(self.pos_of_current_marble, pos_to_move, self.num_of_marbles);
+    }
+
+    fn max_score(&self) -> Score {
+        return *self.players.iter().max().unwrap();
+    }
+
+    fn add_marble() {
+
+    }
+}
+
+fn get_position_clockwise(current_position: usize, pos_to_move: usize, num_of_marbles: usize) -> usize {
+    return (current_position + pos_to_move) % num_of_marbles;
 }
 
 fn main() {
@@ -47,5 +64,18 @@ fn main() {
     // init marble game with the first marble in the circle
     let game_state: GameState = GameState::new(num_of_players);
 
-    println!("{}", input_string);
+    println!("{}", get_position_clockwise(0, 2, 1));
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_position_clockwise() {
+        assert_eq!(get_position_clockwise(1, 1, 8), 2);
+        assert_eq!(get_position_clockwise(7, 1, 8), 0);
+    }
+
 }
