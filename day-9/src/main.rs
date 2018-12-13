0c33f64126b39ae35a5108444998d723281380be
Alberto Leal <mail4alberto@gmail.com>
2018-12-12 21:36:03 -0500

    flesh out game state

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,12 +1,18 @@
 // https://adventofcode.com/2018/day/9
 
-type GameState = Vec<i32>;
+struct GameState(Vec<i32>, /* current marble */ usize);
+
+impl GameState {
+    fn new() -> GameState {
+        GameState(vec![0], 0)
+    }
+}
 
 fn main() {
     let input_string = include_str!("input.txt");
 
     // init marble game with the first marble in the circle
-    let game_state: GameState = vec![0];
+    let game_state: GameState = GameState::new();
 
     println!("{}", input_string);
 }
