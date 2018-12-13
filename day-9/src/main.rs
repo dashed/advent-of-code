6e483eff49c0f470e16639e8996211af71b4352b
Alberto Leal <mail4alberto@gmail.com>
2018-12-12 21:43:48 -0500

    parse input

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,18 +1,47 @@
 // https://adventofcode.com/2018/day/9
 
-struct GameState(Vec<i32>, /* current marble */ usize);
+type Score = i32;
+
+struct GameState {
+    state: Vec<i32>,
+    current_marble: usize,
+    players: Vec<Score>,
+}
 
 impl GameState {
-    fn new() -> GameState {
-        GameState(vec![0], 0)
+    fn new(num_of_players: usize) -> GameState {
+        let mut players = Vec::with_capacity(num_of_players);
+
+        for _ in 1..=num_of_players {
+            // each player has an initial score of 0
+            players.push(0);
+        }
+
+        GameState {
+            state: vec![0],
+            current_marble: 0,
+            players,
+        }
     }
 }
 
 fn main() {
     let input_string = include_str!("input.txt");
 
+    let (num_of_players, nth_marble): (usize, i32) = {
+        let inputs: Vec<&str> = input_string.trim().split_whitespace().collect();
+
+        (
+            inputs.get(0).unwrap().parse().unwrap(),
+            inputs.get(6).unwrap().parse().unwrap(),
+        )
+    };
+
+    println!("num_of_players: {}", num_of_players);
+    println!("nth_marble: {}", nth_marble);
+
     // init marble game with the first marble in the circle
-    let game_state: GameState = GameState::new();
+    let game_state: GameState = GameState::new(num_of_players);
 
     println!("{}", input_string);
 }
