b070c3949435520a3753e838854c6abd0868e7a1
Alberto Leal <mail4alberto@gmail.com>
2018-12-14 14:12:29 -0500

    flesh out day 13

--- day-13/src/main.rs
+++ day-13/src/main.rs
@@ -1,3 +1,39 @@
+use std::collections::HashMap;
+
+type Coordinate = (i32, i32);
+
+enum Track {
+    // |
+    Vertical,
+    // -
+    Horizontal,
+    // +
+    Intersection,
+    // top to left /
+    TopToLeft,
+    // top to right \
+    TopToRight,
+    // bottom to left /
+    BottomToLeft,
+    // bottom to right \
+    BottomToRight,
+}
+
+type Map = HashMap<Coordinate, Track>;
+
+enum TurningOption {
+    Left,
+    Straight,
+    Right,
+}
+
+struct Cart {
+    current_position: Coordinate,
+    // when a cart arrives at an intersection, this rule determines the cart's
+    // next destination
+    turning_option: TurningOption,
+}
+
 fn main() {
     println!("Hello, world!");
 }
