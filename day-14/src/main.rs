0e75b255e42f2436b6e4367394f3bf7a76b02d04
Alberto Leal <mail4alberto@gmail.com>
2018-12-16 02:57:05 -0500

    flesh out

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -1,3 +1,31 @@
+struct Elf {
+    position: usize,
+}
+
+fn generate_recipes_from_digits(sum: i32) -> Vec<i32> {
+    return sum
+        .to_string()
+        .chars()
+        .map(|x| -> i32 {
+            return x.to_string().parse().unwrap();
+        })
+        .collect();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let recipes = vec![3, 7];
+
+    let first_elf = Elf { position: 0 };
+
+    let second_elf = Elf { position: 1 };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_recipes_from_digits() {
+        assert_eq!(generate_recipes_from_digits(1234), vec![1, 2, 3, 4]);
+    }
 }
