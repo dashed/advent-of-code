4ff7fb24c9ef7b8d8b0810125c5b10921e02fe02
Alberto Leal <mail4alberto@gmail.com>
2018-12-16 15:56:27 -0500

    improve day 10 by finding the smallest bounding box

--- day-10/src/main.rs
+++ day-10/src/main.rs
@@ -10,13 +10,14 @@ fn substring(this: &str, start: usize, len: usize) -> String {
     this.chars().skip(start).take(len).collect()
 }
 
+#[derive(Debug, Clone)]
 struct Star {
-    position: (i32, i32),
-    velocity: (i32, i32),
+    position: (i64, i64),
+    velocity: (i64, i64),
 }
 
 impl Star {
-    fn wait_for(&mut self, seconds: i32) {
+    fn wait_for(&mut self, seconds: i64) {
         let (x, y) = self.position;
         let (x_velocity, y_velocity) = self.velocity;
 
@@ -27,16 +28,16 @@ impl Star {
 fn parse_input_to_star(input: &str) -> Star {
     let input: Vec<&str> = input.trim().split("velocity").map(|x| x.trim()).collect();
 
-    let position: (i32, i32) = {
+    let position: (i64, i64) = {
         let raw_string = input.get(0).unwrap();
 
         let skip = "position=<".len();
         let end = raw_string.len() - skip - 1;
         let raw_string = substring(raw_string, skip, end);
 
-        let tokens: Vec<i32> = raw_string
+        let tokens: Vec<i64> = raw_string
             .split(',')
-            .map(|x| -> i32 {
+            .map(|x| -> i64 {
                 return x.trim().parse().unwrap();
             })
             .collect();
@@ -44,16 +45,16 @@ fn parse_input_to_star(input: &str) -> Star {
         (*tokens.get(0).unwrap(), *tokens.get(1).unwrap())
     };
 
-    let velocity: (i32, i32) = {
+    let velocity: (i64, i64) = {
         let raw_string = input.get(1).unwrap();
 
         let skip = "=<".len();
         let end = raw_string.len() - skip - 1;
         let raw_string = substring(raw_string, skip, end);
 
-        let tokens: Vec<i32> = raw_string
+        let tokens: Vec<i64> = raw_string
             .split(',')
-            .map(|x| -> i32 {
+            .map(|x| -> i64 {
                 return x.trim().parse().unwrap();
             })
             .collect();
@@ -64,20 +65,75 @@ fn parse_input_to_star(input: &str) -> Star {
     Star { position, velocity }
 }
 
+fn find_smallest_area(stars: Vec<Star>) -> Vec<Star> {
+    // let wait_for = 10124;
+    let mut wait_for = 10000;
+
+    let mut best_stars: Vec<Star> = stars
+        .into_iter()
+        .map(|mut star| {
+            star.wait_for(wait_for);
+            star
+        })
+        .collect();
+
+    let mut smallest_area = None;
+
+    loop {
+        let stars: Vec<Star> = best_stars
+            .iter()
+            .map(|star| {
+                let mut star = star.clone();
+                star.wait_for(1);
+                star
+            })
+            .collect();
+
+        let max_x = stars.iter().map(|star| star.position.0).max().unwrap();
+        let min_x = stars.iter().map(|star| star.position.0).min().unwrap();
+        let max_y = stars.iter().map(|star| star.position.1).max().unwrap();
+        let min_y = stars.iter().map(|star| star.position.1).min().unwrap();
+
+        let width = max_x - min_x + 1;
+        let height = max_y - min_y + 1;
+
+        assert!(width >= 0);
+        assert!(height >= 0);
+        let area: i64 = height * width;
+
+        match smallest_area {
+            None => {
+                smallest_area = Some(area);
+            }
+            Some(best_area) => {
+                if area > best_area {
+                    println!("The stars align if you wait for {} seconds.", wait_for - 1);
+                    return best_stars;
+                }
+
+                best_stars = stars;
+                smallest_area = Some(area);
+            }
+        }
+
+        wait_for += 1;
+    }
+}
+
 fn main() {
-    let wait_for = 10124;
+    // let wait_for = 10124;
 
     let input_string = include_str!("input.txt");
 
-    let inputs: Vec<Star> = input_string
-        .trim()
-        .lines()
-        .map(|x| parse_input_to_star(x))
-        .map(|mut x| {
-            x.wait_for(wait_for);
-            x
-        })
-        .collect();
+    let inputs: Vec<Star> = {
+        let initial_stars: Vec<Star> = input_string
+            .trim()
+            .lines()
+            .map(|x| parse_input_to_star(x))
+            .collect();
+
+        find_smallest_area(initial_stars)
+    };
 
     let max_x = inputs.iter().map(|star| star.position.0).max().unwrap();
     let min_x = inputs.iter().map(|star| star.position.0).min().unwrap();
