dbeb401d26456a6af6604226c22e8b144a7225ea
Alberto Leal <mail4alberto@gmail.com>
2018-12-16 03:17:44 -0500

    refactor

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -5,8 +5,13 @@ struct Elf {
 }
 
 impl Elf {
-    fn get_recipe(&self, receipes: &Vec<Recipe>) -> Recipe {
-        return *receipes.get(self.position).unwrap();
+    fn get_recipe(&self, scoreboard: &Vec<Recipe>) -> Recipe {
+        return *scoreboard.get(self.position).unwrap();
+    }
+
+    fn move_to_new_position(&mut self, current_recipe: Recipe, scoreboard: &Vec<Recipe>) {
+        let num_of_moves: usize = (1 + current_recipe) as usize;
+        self.position = (self.position + num_of_moves) % scoreboard.len();
     }
 }
 
@@ -20,26 +25,48 @@ fn generate_recipes_from_digits(mixed_recipe: Recipe) -> Vec<Recipe> {
         .collect();
 }
 
-fn main() {
+fn part_1(part_1_input: usize) -> String {
+    let part_1_guard = part_1_input + 10;
+
     let mut scoreboard = vec![3, 7];
 
-    let first_elf = Elf { position: 0 };
-    let second_elf = Elf { position: 1 };
+    let mut first_elf = Elf { position: 0 };
+    let mut second_elf = Elf { position: 1 };
 
-    // each elf gets a recipe from the scoreboard
+    loop {
+        // each elf gets a recipe from the scoreboard
 
-    let recipe_1 = first_elf.get_recipe(&scoreboard);
-    let recipe_2 = second_elf.get_recipe(&scoreboard);
+        let recipe_1 = first_elf.get_recipe(&scoreboard);
+        let recipe_2 = second_elf.get_recipe(&scoreboard);
 
-    // each elf combine their current recipes
+        // each elf combine their current recipes
 
-    let result = generate_recipes_from_digits(recipe_1 + recipe_2);
+        let result = generate_recipes_from_digits(recipe_1 + recipe_2);
 
-    // the resulting recipes are added to the scoreboard
+        // the resulting recipes are added to the scoreboard
 
-    scoreboard.extend(result);
+        scoreboard.extend(result);
 
-    println!("{:?}", scoreboard);
+        first_elf.move_to_new_position(recipe_1, &scoreboard);
+        second_elf.move_to_new_position(recipe_2, &scoreboard);
+
+        if scoreboard.len() >= part_1_guard {
+            let result: String = scoreboard
+                .iter()
+                .skip(part_1_input)
+                .take(10)
+                .map(|x| x.to_string())
+                .collect::<Vec<String>>()
+                .join("");
+
+            return result;
+        }
+    }
+}
+
+fn main() {
+    let result = part_1(5);
+    println!("Part 1: {}", result);
 }
 
 #[cfg(test)]
