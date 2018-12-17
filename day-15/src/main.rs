b3a6a848bd9e2e94eecae8372feaf2ff5713c02c
Alberto Leal <mail4alberto@gmail.com>
2018-12-17 05:02:32 -0500

    enum unit and docs

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -15,6 +15,16 @@ enum MapState {
 
 type Map = HashMap<Coordinate, MapState>;
 
+enum Unit {
+    Goblin,
+    Elf
+}
+
+// combat begins in a series of rounds
+// in each round, a unit takes a turn, resolving all of its actions before completing their turn
+// On each unit's turn, it tries to move into range of an enemy (if it isn't already) and then attack (if it is in range).
+// invariant: units cannot attack nor move diagonally
+
 fn main() {
 
     // ensures reading order is satisfied
