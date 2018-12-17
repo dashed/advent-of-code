c824b9d3d26f1be7dc44c49824dd384a6767ef91
Alberto Leal <mail4alberto@gmail.com>
2018-12-17 04:55:34 -0500

    parse input

--- day-15/src/main.rs
+++ day-15/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+
+    // ensures reading order is satisfied
+    assert!((0, 0) < (1, 0));
+    assert!((0, 0) < (0, 1));
+    assert!((0, 0) < (1, 1));
+    assert!((1, 0) < (1, 1));
+    assert!((0, 0) < (1, 1));
+
+    let input_string = include_str!("input.txt");
+
+    println!("{:?}", input_string);
 }
