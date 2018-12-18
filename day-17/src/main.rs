da4b60e5852a1e3fedae47dc8456a25adb1513a7
Alberto Leal <mail4alberto@gmail.com>
2018-12-18 13:50:58 -0500

    flesh out

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -1,5 +1,43 @@
 // https://adventofcode.com/2018/day/17
 
+// code
+
+type Coordinate = (i32, i32);
+
+trait Transitions {
+    fn down(&self) -> Coordinate;
+    fn left(&self) -> Coordinate;
+    fn right(&self) -> Coordinate;
+}
+
+impl Transitions for Coordinate {
+    fn down(&self) -> Coordinate {
+        let (x, y) = self;
+        return (*x, y + 1);
+    }
+
+    fn left(&self) -> Coordinate {
+        let (x, y) = self;
+        return (x - 1, *y);
+    }
+
+    fn right(&self) -> Coordinate {
+        let (x, y) = self;
+        return (x + 1, *y);
+    }
+}
+
+enum Water {
+    AtRest,
+    Reachable,
+}
+
+enum MapState {
+    Clay,
+    Sand,
+    Water(Water),
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
