c196fe1f0f40d1c5dc6cea8ae1a4b81a95bb47f6
Alberto Leal <mail4alberto@gmail.com>
2018-12-18 09:57:58 -0500

    init

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -1,5 +1,44 @@
 // https://adventofcode.com/2018/day/15
 
+#[derive(Debug, Clone)]
+enum RegisterID {
+    Zero,
+    One,
+    Two,
+    Three,
+}
+
+struct Registers(i32, i32, i32, i32);
+
+struct OpcodeInstruction(
+    i32,        /* input 1 */
+    i32,        /* input 2 */
+    RegisterID, /* output register */
+);
+
+impl OpcodeInstruction {
+    fn output_register(&self) -> RegisterID {
+        return self.2.clone();
+    }
+}
+
+enum Opcode {
+    Addr,
+}
+
+impl Opcode {
+    fn process(&self, registers: Registers, instruction: OpcodeInstruction) -> Registers {
+        match self {
+            Opcode::Addr => {
+                // addr (add register) stores into register C the result of adding register A and register B.
+                let register_c = instruction.output_register();
+            }
+        }
+
+        return registers;
+    }
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
