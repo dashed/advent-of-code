b9c99eda2abfa49ed9087b97334768bbb2b2d412
Alberto Leal <mail4alberto@gmail.com>
2018-12-18 11:33:57 -0500

    bitwise and

--- day-16/src/main.rs
+++ day-16/src/main.rs
@@ -93,6 +93,8 @@ enum Opcode {
     Addi,
     Mulr,
     Muli,
+    Banr,
+    Bani,
 }
 
 impl Opcode {
@@ -101,8 +103,12 @@ impl Opcode {
 
         set.insert(Opcode::Addr);
         set.insert(Opcode::Addi);
+
         set.insert(Opcode::Mulr);
         set.insert(Opcode::Muli);
+
+        set.insert(Opcode::Banr);
+        set.insert(Opcode::Bani);
 
         return set;
     }
@@ -184,6 +190,43 @@ impl Opcode {
                 let value_b = instruction.input_b();
 
                 let result = value_a * value_b;
+
+                let register_c = instruction.output_register();
+                registers_before.set(register_c, result);
+            }
+            Opcode::Banr => {
+                // banr (bitwise AND register) stores into register C the result of the bitwise AND of register A and register B.
+
+                let register_a = RegisterID::into_register_id(instruction.input_a());
+                if register_a.is_none() {
+                    return false;
+                }
+
+                let register_b = RegisterID::into_register_id(instruction.input_b());
+                if register_b.is_none() {
+                    return false;
+                }
+
+                let value_a = registers_before.get(register_a.unwrap());
+                let value_b = registers_before.get(register_b.unwrap());
+
+                let register_c = instruction.output_register();
+
+                let result = value_a & value_b;
+                registers_before.set(register_c, result);
+            }
+            Opcode::Bani => {
+                // bani (bitwise AND immediate) stores into register C the result of the bitwise AND of register A and value B.
+
+                let register_a = RegisterID::into_register_id(instruction.input_a());
+                if register_a.is_none() {
+                    return false;
+                }
+
+                let value_a = registers_before.get(register_a.unwrap());
+                let value_b = instruction.input_b();
+
+                let result = value_a & value_b;
 
                 let register_c = instruction.output_register();
                 registers_before.set(register_c, result);
