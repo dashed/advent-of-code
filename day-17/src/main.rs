0848929bcc48c21b89d3b76da2fd0c68680460e5
Alberto Leal <mail4alberto@gmail.com>
2018-12-18 20:20:35 -0500

    flesh out day 17

--- day-17/src/main.rs
+++ day-17/src/main.rs
@@ -44,8 +44,60 @@ enum MapState {
 
 type Terrain = HashMap<Coordinate, MapState>;
 
+struct Map {
+    terrain: Terrain,
+}
+
+impl Map {
+    fn new() -> Self {
+        Map {
+            terrain: HashMap::new(),
+        }
+    }
+
+    fn insert_clay(&mut self, clay_coordinate: &Coordinate) {
+        self.terrain.insert(*clay_coordinate, MapState::Clay);
+    }
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
-    println!("{}", input_string);
+    // parse positions of clay
+
+    let clay_coordinates: Vec<Coordinate> = input_string
+        .trim()
+        .lines()
+        .map(|line| {
+            let target = line.split("..").next().unwrap().trim();
+            let coord: Vec<i32> = target
+                .split(",")
+                .map(|s| {
+                    return s.trim().split("=").skip(1).next().unwrap().trim();
+                })
+                .map(|s| {
+                    return s.parse::<i32>().unwrap();
+                })
+                .collect();
+
+            assert!(coord.len() == 2);
+
+            let coord: Coordinate = (coord[0], coord[1]);
+
+            return coord;
+        })
+        .collect();
+
+    // add clay to terrain
+
+    let mut map = Map::new();
+
+    // let terrain = HashMap::new();
+
+    for coordinate in clay_coordinates {
+        // println!("{:?}", coordinate);
+        map.insert_clay(&coordinate);
+    }
+
+    // println!("{}", input_string);
 }
