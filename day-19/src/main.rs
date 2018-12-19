86607d4b5ebce248541773cd9c5e38921aa6eede
Alberto Leal <mail4alberto@gmail.com>
2018-12-19 13:07:15 -0500

    flesh out

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -2,6 +2,56 @@
 
 // code
 
+#[derive(Debug, Clone)]
+enum RegisterID {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+}
+
+struct Registers(i32, i32, i32, i32);
+
+enum Opcode {
+    Addr,
+    Addi,
+    Mulr,
+    Muli,
+    Banr,
+    Bani,
+    Borr,
+    Bori,
+    Setr,
+    Seti,
+    Gtir,
+    Gtri,
+    Gtrr,
+    Eqir,
+    Eqri,
+    Eqrr,
+}
+
+struct OpcodeInstruction(
+    Opcode,
+    i32,        /* input A */
+    i32,        /* input B */
+    RegisterID, /* output register */
+);
+
+struct Program {
+    // current value of the instruction pointer
+    instruction_pointer: i32,
+    // indicates the register that the instruction pointer is bound to
+    instruction_pointer_bound: RegisterID,
+    registers: Registers,
+}
+
+impl Program {
+    // add code here
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
