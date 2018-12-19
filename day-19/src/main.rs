85da659aa2855415745a329263b2c220576a7737
Alberto Leal <mail4alberto@gmail.com>
2018-12-19 13:25:19 -0500

    flesh out

--- day-19/src/main.rs
+++ day-19/src/main.rs
@@ -46,22 +46,64 @@ struct OpcodeInstruction(
     RegisterID, /* output register */
 );
 
+enum Status {
+    Halted,
+    NotHalted,
+}
+
 struct Program {
     // current value of the instruction pointer
-    instruction_pointer: i32,
+    instruction_pointer: usize,
     // indicates the register that the instruction pointer is bound to
     instruction_pointer_bound: RegisterID,
     registers: Registers,
+
+    instructions: Vec<OpcodeInstruction>,
 }
 
 impl Program {
-    fn new(instruction_pointer_bound: RegisterID) -> Self {
+    fn new(instruction_pointer_bound: RegisterID, instructions: Vec<OpcodeInstruction>) -> Self {
         Program {
             // The instruction pointer starts at 0.
             instruction_pointer: 0,
             instruction_pointer_bound,
             registers: Registers::new(),
+            instructions,
         }
+    }
+
+    fn run_program(&mut self) {
+        loop {
+            let result = self.execute_instruction();
+            match result {
+                Status::Halted => {
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    fn execute_instruction(&mut self) -> Status {
+        // get next instruction
+        let instruction = self.instructions.get(self.instruction_pointer);
+
+        if instruction.is_none() {
+            // If the instruction pointer ever causes the device to attempt to load
+            // an instruction outside the instructions defined in the program, the program instead immediately halts.
+            return Status::Halted;
+        }
+
+        // TODO: write the value in instruction pointer to the bound register
+
+        // TODO: execute instruction
+
+        // TODO: write bound register back to the instruction poiinter
+
+        // after the instruction has executed, add one to the instruction pointer
+        self.instruction_pointer += 1;
+
+        return Status::NotHalted;
     }
 }
 
