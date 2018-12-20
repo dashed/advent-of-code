fbb1c0cf3adcc45a5d945bf25570482a6dff8b5a
Alberto Leal <mail4alberto@gmail.com>
2018-12-20 00:06:54 -0500

    flesh out

--- day-20/src/main.rs
+++ day-20/src/main.rs
@@ -1,3 +1,42 @@
+// https://adventofcode.com/2018/day/20
+
+enum Tile {
+    Door,
+    Tile,
+    Room
+}
+
+type Coordinate = (i32, i32);
+
+trait Transitions {
+    fn up(&self) -> Coordinate;
+    fn down(&self) -> Coordinate;
+    fn left(&self) -> Coordinate;
+    fn right(&self) -> Coordinate;
+}
+
+impl Transitions for Coordinate {
+    fn up(&self) -> Coordinate {
+        let (x, y) = self;
+        return (*x, y - 1);
+    }
+
+    fn down(&self) -> Coordinate {
+        let (x, y) = self;
+        return (*x, y + 1);
+    }
+
+    fn left(&self) -> Coordinate {
+        let (x, y) = self;
+        return (x - 1, *y);
+    }
+
+    fn right(&self) -> Coordinate {
+        let (x, y) = self;
+        return (x + 1, *y);
+    }
+}
+
 fn main() {
 
     let input_string = include_str!("input.txt");
