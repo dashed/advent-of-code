2a50f8ef89bfb6d58e225cedd9c62770c7298f3b
Alberto Leal <mail4alberto@gmail.com>
2018-12-20 00:16:23 -0500

    flesh out

--- day-20/src/main.rs
+++ day-20/src/main.rs
@@ -1,44 +1,71 @@
 // https://adventofcode.com/2018/day/20
 
-enum Tile {
-    Door,
-    Tile,
-    Room
+// imports
+
+use std::collections::HashMap;
+use std::collections::HashSet;
+
+// code
+
+enum OpenDirections {
+    North,
+    South,
+    West,
+    East,
+}
+
+impl OpenDirections {
+    fn from_char(d: char) -> Self {
+        match d {
+            'N' => OpenDirections::North,
+            'S' => OpenDirections::South,
+            'W' => OpenDirections::West,
+            'E' => OpenDirections::East,
+            _ => {
+                unreachable!();
+            }
+        }
+    }
 }
 
 type Coordinate = (i32, i32);
 
 trait Transitions {
-    fn up(&self) -> Coordinate;
-    fn down(&self) -> Coordinate;
-    fn left(&self) -> Coordinate;
-    fn right(&self) -> Coordinate;
+    fn north(&self) -> Coordinate;
+    fn south(&self) -> Coordinate;
+    fn west(&self) -> Coordinate;
+    fn east(&self) -> Coordinate;
 }
 
 impl Transitions for Coordinate {
-    fn up(&self) -> Coordinate {
+    fn north(&self) -> Coordinate {
         let (x, y) = self;
         return (*x, y - 1);
     }
 
-    fn down(&self) -> Coordinate {
+    fn south(&self) -> Coordinate {
         let (x, y) = self;
         return (*x, y + 1);
     }
 
-    fn left(&self) -> Coordinate {
+    fn west(&self) -> Coordinate {
         let (x, y) = self;
         return (x - 1, *y);
     }
 
-    fn right(&self) -> Coordinate {
+    fn east(&self) -> Coordinate {
         let (x, y) = self;
         return (x + 1, *y);
     }
 }
 
-fn main() {
+struct Map {
+    // a coordinate indicates a room;
+    // and from this room indicates possible directions to go into
+    map: HashMap<Coordinate, HashSet<OpenDirections>>,
+}
 
+fn main() {
     let input_string = include_str!("input.txt");
 
     println!("{}", input_string);
