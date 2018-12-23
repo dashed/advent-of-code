b26e257257d027c4f984cc1209f13ec1478ca992
Alberto Leal <mail4alberto@gmail.com>
2018-12-23 08:26:34 -0500

    🎄

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -9,6 +9,7 @@ use std::collections::HashMap;
 type Coordinate = (i32, i32);
 type GeologicIndex = i32;
 type RiskLevel = i32;
+type ErosionLevel = i32;
 
 const MOUTH_OF_CAVE: Coordinate = (0, 0);
 
@@ -43,6 +44,46 @@ impl Cave {
         geologic_indices.insert(target, 0);
 
         Cave { geologic_indices }
+    }
+
+    fn get_erosion_level(&self, coord: &Coordinate) -> ErosionLevel {
+        return 0;
+    }
+
+    fn get_geologic_index(&mut self, coord: &Coordinate) -> GeologicIndex {
+        match self.geologic_indices.get(coord) {
+            Some(index) => {
+                return *index;
+            }
+            None => {
+                // generate one
+            }
+        }
+
+        let (x, y) = coord;
+        let mut geologic_index = 0;
+
+        // If the region's Y coordinate is 0,
+        // the geologic index is its X coordinate times 16807.
+        if *y == 0 {
+            geologic_index = x * 16807;
+        }
+
+        // If the region's X coordinate is 0,
+        // the geologic index is its Y coordinate times 48271.
+        if *x == 0 {
+            geologic_index = y * 16807;
+        }
+
+        // Otherwise, the region's geologic index is
+        // the result of multiplying the erosion levels of the regions at X-1,Y and X,Y-1.
+
+        geologic_index =
+            self.get_erosion_level(&(x - 1, *y)) * self.get_erosion_level(&(*x, y - 1));
+
+        self.geologic_indices.insert(*coord, geologic_index);
+
+        return geologic_index;
     }
 }
 
