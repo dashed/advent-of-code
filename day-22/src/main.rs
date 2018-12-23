ea946d0071603f45f89520720d2353dc29fae36b
Alberto Leal <mail4alberto@gmail.com>
2018-12-23 08:53:12 -0500

    🎄

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -28,10 +28,20 @@ impl RegionType {
             RegionType::Narrow => 2,
         }
     }
+
+    fn to_string(&self) -> String {
+        let result = match self {
+            RegionType::Rocky => ".",
+            RegionType::Wet => "=",
+            RegionType::Narrow => "|",
+        };
+        return result.to_string();
+    }
 }
 
 struct Cave {
     depth: Depth,
+    target: Coordinate,
     geologic_indices: HashMap<Coordinate, GeologicIndex>,
 }
 
@@ -47,12 +57,30 @@ impl Cave {
 
         Cave {
             depth,
+            target,
             geologic_indices,
         }
     }
 
-    fn get_erosion_level(&self, coord: &Coordinate) -> ErosionLevel {
-        return 0;
+    fn get_risk_level(&mut self, coord: &Coordinate) -> RiskLevel {
+        return self.get_region_type(coord).risk_level();
+    }
+
+    fn get_region_type(&mut self, coord: &Coordinate) -> RegionType {
+        let result = self.get_erosion_level(coord) % 3;
+
+        match result {
+            0 => RegionType::Rocky,
+            1 => RegionType::Wet,
+            2 => RegionType::Narrow,
+            _ => {
+                unreachable!();
+            }
+        }
+    }
+
+    fn get_erosion_level(&mut self, coord: &Coordinate) -> ErosionLevel {
+        return (self.get_geologic_index(coord) + self.depth) % 20183;
     }
 
     fn get_geologic_index(&mut self, coord: &Coordinate) -> GeologicIndex {
@@ -66,37 +94,99 @@ impl Cave {
         }
 
         let (x, y) = coord;
-        let mut geologic_index = 0;
-
-        // If the region's Y coordinate is 0,
-        // the geologic index is its X coordinate times 16807.
-        if *y == 0 {
-            geologic_index = x * 16807;
-        }
-
-        // If the region's X coordinate is 0,
-        // the geologic index is its Y coordinate times 48271.
-        if *x == 0 {
-            geologic_index = y * 16807;
-        }
-
-        // Otherwise, the region's geologic index is
-        // the result of multiplying the erosion levels of the regions at X-1,Y and X,Y-1.
-
-        geologic_index =
-            self.get_erosion_level(&(x - 1, *y)) * self.get_erosion_level(&(*x, y - 1));
+        let geologic_index = if *y == 0 {
+            // If the region's Y coordinate is 0,
+            // the geologic index is its X coordinate times 16807.
+            x * 16807
+        } else if *x == 0 {
+            // If the region's X coordinate is 0,
+            // the geologic index is its Y coordinate times 48271.
+            y * 16807
+        } else {
+            // Otherwise, the region's geologic index is
+            // the result of multiplying the erosion levels of the regions at X-1,Y and X,Y-1.
+            self.get_erosion_level(&(x - 1, *y)) * self.get_erosion_level(&(*x, y - 1))
+        };
 
         self.geologic_indices.insert(*coord, geologic_index);
 
         return geologic_index;
     }
+
+    fn to_string(&mut self) -> String {
+        let (target_x, target_y) = self.target;
+
+        let mut map_string: Vec<String> = vec![];
+
+        for y in 0..=target_y {
+            let mut row_string = String::from("");
+
+            for x in 0..=target_x {
+                let coord = (x, y);
+
+                if coord == MOUTH_OF_CAVE {
+                    row_string.push_str("M");
+                    continue;
+                }
+
+                if coord == self.target {
+                    row_string.push_str("T");
+                    continue;
+                }
+
+                let result = self.get_region_type(&coord).to_string();
+
+                row_string.push_str(&result);
+            }
+
+            map_string.push(row_string);
+        }
+
+        return map_string.join("\n");
+    }
+}
+
+fn part_1(depth: Depth, target: Coordinate) -> RiskLevel {
+    let (target_x, target_y) = target;
+
+    let mut cave = Cave::new(depth, target);
+
+    let mut total_risk: RiskLevel = 0;
+
+    for x in 0..target_x {
+        for y in 0..target_y {
+            let coord = (x, y);
+
+            total_risk += cave.get_risk_level(&coord);
+        }
+    }
+
+    println!("{}", cave.to_string());
+
+    return total_risk;
 }
 
 fn main() {
     // input
 
-    let depth = 4002;
-    let target: Coordinate = (5, 746);
+    let part_1 = part_1(510, (10, 10));
+    println!("Part 1: {}", part_1);
 
-    let cave = Cave::new(depth, target);
+    // let depth = 4002;
+    // let target: Coordinate = (5, 746);
+
+    // let cave = Cave::new(depth, target);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1() {
+        let part_1 = part_1(510, (10, 10));
+
+        assert_eq!(part_1, 114);
+    }
+
 }
