272d08a80baf973b1af21a0b220a9bae8b601638
Alberto Leal <mail4alberto@gmail.com>
2018-12-23 08:08:47 -0500

    flesh out

--- day-22/src/main.rs
+++ day-22/src/main.rs
@@ -1,3 +1,29 @@
+// https://adventofcode.com/2018/day/22
+
+// imports
+
+use std::collections::HashMap;
+
+// code
+
+type Coordinate = (i32, i32);
+type GeologicIndex = i32;
+
+const MOUTH_OF_CAVE: Coordinate = (0, 0);
+
+enum RegionType {
+    Rocky,
+    Narrow,
+    Wet,
+}
+
+struct Cave {
+    geologic_indices: HashMap<Coordinate, GeologicIndex>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // input
+
+    let depth = 4002;
+    let target: Coordinate = (5, 746);
 }
