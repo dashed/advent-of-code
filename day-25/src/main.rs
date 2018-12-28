f6fe393fb79639c49aed916ec9f39ecfd9d33169
Alberto Leal <mail4alberto@gmail.com>
2018-12-28 11:14:46 -0500

    🎄

--- day-25/src/main.rs
+++ day-25/src/main.rs
@@ -18,6 +18,7 @@ fn get_manhattan_distance(start: Coordinate, end: Coordinate) -> Distance {
 
 type Coordinate = (i32, i32, i32, i32);
 
+#[derive(Debug, Clone)]
 struct Constellation {
     points: HashSet<Coordinate>,
 }
@@ -28,6 +29,23 @@ impl Constellation {
         points.insert(point);
 
         return Constellation { points };
+    }
+
+    fn merge(self, other: Self) -> Self {
+        let mut new_points = self.points.clone();
+        new_points.extend(other.points);
+
+        return Constellation { points: new_points };
+    }
+
+    fn can_merge(&mut self, other: &Self) -> bool {
+        for current_point in &self.points {
+            if other.can_add(*current_point) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     fn can_add(&self, new_point: Coordinate) -> bool {
@@ -80,13 +98,51 @@ fn part_1(input_string: &str) -> usize {
         constellations.push(constellation);
     }
 
+    // merge constallations
+
+    loop {
+        if constellations.len() <= 1 {
+            break;
+        }
+
+        let mut merge_occurred = false;
+
+        let mut new_constellations = vec![];
+
+        let last_constellation: Constellation = constellations.pop().unwrap();
+
+        while let Some(mut constellation) = constellations.pop() {
+            // find a constellation that can merge with last_constellation
+            if constellation.can_merge(&last_constellation) {
+                merge_occurred = true;
+                let new_constellation = constellation.merge(last_constellation.clone());
+                new_constellations.push(new_constellation);
+                break;
+            }
+
+            new_constellations.push(constellation);
+        }
+
+        // add remaining constellations that didn't merge with last_constellation
+        new_constellations.extend(constellations);
+
+        constellations = new_constellations;
+
+        if !merge_occurred {
+            constellations.push(last_constellation);
+            break;
+        }
+    }
+
     return constellations.len();
 }
 
 fn main() {
 
+    let input_string = include_str!("input.txt");
+
     // not: 616 (too high)
-    // println!("Part 1: {}", constellations.len());
+    println!("Part 1: {}", part_1(input_string));
 }
 
 #[cfg(test)]
