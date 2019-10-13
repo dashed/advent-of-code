29372808e440cf2712e4794f442d7766d03bcb15
Alberto Leal <mail4alberto@gmail.com>
2019-10-13 01:29:14 -0400

    parse input

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -2,8 +2,41 @@
 
 // code
 
+#[derive(Debug)]
+struct Dimensions {
+    length: u32,
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
-    println!("{}", input_string);
+    let dimensions = parse_input(input_string);
+
+    println!("{:?}", dimensions);
+}
+
+fn parse_input(input_string: &str) -> Vec<Dimensions> {
+    let mut output: Vec<Dimensions> = vec![];
+
+    for input in input_string.trim().lines() {
+        let input = input.trim();
+
+        let coords: Vec<u32> = input
+            .split("x")
+            .map(|x| x.trim())
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+
+        let result = Dimensions {
+            length: coords[0],
+            width: coords[1],
+            height: coords[2],
+        };
+
+        output.push(result);
+    }
+
+    return output;
 }
