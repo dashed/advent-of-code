2e33a4185bc3e12de3ccc6080245b78a39dfcd77
Alberto Leal <mail4alberto@gmail.com>
2019-12-02 00:55:32 -0500

    day 2 2019

--- day-02-2019/src/main.rs
+++ day-02-2019/src/main.rs
@@ -1,8 +1,8 @@
 // https://adventofcode.com/2019/day/2
 
-fn main() {
-    let input_string = include_str!("input.txt");
+use rayon::prelude::*;
 
+fn run_computer(input_string: String, noun: u32, verb: u32) -> u32 {
     let mut inputs: Vec<u32> = input_string
         .trim()
         .split(',')
@@ -11,13 +11,8 @@ fn main() {
         })
         .collect();
 
-    // restore the gravity assist program (your puzzle input) to the "1202 program alarm" state
-    // it had just before the last computer caught fire. To do this, before running the program,
-    // replace position 1 with the value 12 and replace position 2 with the value 2.
-    inputs[1] = 12;
-    inputs[2] = 2;
-
-    // Part 1
+    inputs[1] = noun;
+    inputs[2] = verb;
 
     let mut current_instruction_pointer: usize = 0;
 
@@ -27,15 +22,19 @@ fn main() {
         match opcode {
             1 => {
                 // add opcode
-                let arg_1 = inputs[current_instruction_pointer + 1];
-                let arg_2 = inputs[current_instruction_pointer + 2];
+                let pos_arg_1 = inputs[current_instruction_pointer + 1];
+                let arg_1 = inputs[pos_arg_1 as usize];
+                let pos_arg_2 = inputs[current_instruction_pointer + 2];
+                let arg_2 = inputs[pos_arg_2 as usize];
                 let position_result = inputs[current_instruction_pointer + 3];
                 inputs[position_result as usize] = arg_1 + arg_2;
             }
             2 => {
                 // multiply opcode
-                let arg_1 = inputs[current_instruction_pointer + 1];
-                let arg_2 = inputs[current_instruction_pointer + 2];
+                let pos_arg_1 = inputs[current_instruction_pointer + 1];
+                let arg_1 = inputs[pos_arg_1 as usize];
+                let pos_arg_2 = inputs[current_instruction_pointer + 2];
+                let arg_2 = inputs[pos_arg_2 as usize];
                 let position_result = inputs[current_instruction_pointer + 3];
                 inputs[position_result as usize] = arg_1 * arg_2;
             }
@@ -54,7 +53,69 @@ fn main() {
         current_instruction_pointer = current_instruction_pointer + 4;
     }
 
-    // not 116
+    return inputs[0];
+}
 
-    println!("Part 1: {}", inputs[0]);
+fn part_2(input_string: String) {
+    let noun_range: Vec<u32> = (0..=99).into_iter().collect();
+    let verb_range: Vec<u32> = (0..=99).into_iter().collect();
+
+    let result: Option<(u32, u32)> = noun_range
+        .into_par_iter()
+        .map(|noun| -> Option<(u32, u32)> {
+            let result = verb_range
+                .par_iter()
+                .map(|verb| -> Option<(u32, u32)> {
+                    let result = run_computer(input_string.clone(), noun, *verb);
+
+                    if result == 19690720 {
+                        return Some((noun, *verb));
+                    }
+
+                    return None;
+                })
+                .find_first(|result: &Option<(u32, u32)>| {
+                    return result.is_some();
+                });
+
+            if result.is_some() {
+                return result.unwrap();
+            }
+
+            return None;
+        })
+        .find_first(|result: &Option<(u32, u32)>| {
+            return result.is_some();
+        })
+        .unwrap();
+
+    match result {
+        Some((noun, verb)) => {
+            println!(
+                "Part 2: noun:{} verb:{} => {}",
+                noun,
+                verb,
+                100 * noun + verb
+            );
+        }
+        None => {
+            println!("oh no");
+        }
+    }
+}
+
+fn main() {
+    let input_string = include_str!("input.txt");
+    // let input_string = "1,9,10,3,2,3,11,0,99,30,40,50";
+
+    // Part 1
+
+    // restore the gravity assist program (your puzzle input) to the "1202 program alarm" state
+    // it had just before the last computer caught fire. To do this, before running the program,
+    // replace position 1 with the value 12 and replace position 2 with the value 2.
+    println!("Part 1: {}", run_computer(input_string.to_string(), 12, 2));
+
+    // Part 2
+
+    part_2(input_string.to_string());
 }
