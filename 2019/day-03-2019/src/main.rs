af05b4afa1b4e1991ec9db47bd78820618fd5fed
Alberto Leal <mail4alberto@gmail.com>
2019-12-03 02:11:41 -0500

    input

--- 2019/day-03-2019/src/main.rs
+++ 2019/day-03-2019/src/main.rs
@@ -1,3 +1,12 @@
+// https://adventofcode.com/2019/day/3
+
 fn main() {
-    println!("Hello, world!");
+
+    let input_string = include_str!("input.txt");
+
+    // Part 1
+
+    // let inputs: Vec<&str> = input_string.trim().split_whitespace().collect();
+
+    println!("{:?}", input_string);
 }
