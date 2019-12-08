b89639da0a670efd721af170895db09618177b93
Alberto Leal <mail4alberto@gmail.com>
2019-12-08 02:21:21 -0500

    computer

--- 2019/day-05-2019/src/main.rs
+++ 2019/day-05-2019/src/main.rs
@@ -1,5 +1,27 @@
 // https://adventofcode.com/2019/day/5
 
+struct Computer {
+    current_instruction_pointer: usize,
+    tape: Vec<i32>,
+}
+
+impl Computer {
+    fn new(input_string: String) -> Self {
+        let inputs: Vec<i32> = input_string
+            .trim()
+            .split(',')
+            .map(|value| -> i32 {
+                return value.parse().unwrap();
+            })
+            .collect();
+
+        Computer {
+            current_instruction_pointer: 0,
+            tape: inputs,
+        }
+    }
+}
+
 fn main() {
     let input_string = include_str!("input.txt");
 
