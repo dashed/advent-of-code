be53635465c8701e781be587e2c1528abeb2141a
Alberto Leal <mail4alberto@gmail.com>
2020-12-07 08:00:33 -0500

    day 2: part 2

--- 2020/day-02-2020/src/main.rs
+++ 2020/day-02-2020/src/main.rs
@@ -6,6 +6,10 @@ fn main() {
     // Part 1
 
     println!("Part 1: {}", part_1(puzzle_inputs.clone()));
+
+    // Part 2
+
+    println!("Part 2: {}", part_2(puzzle_inputs));
 }
 
 #[derive(Clone)]
@@ -17,9 +21,26 @@ struct PasswordPolicy {
 }
 
 impl PasswordPolicy {
-    fn is_valid(&self) -> bool {
+    fn is_valid_old_policy(&self) -> bool {
         let counts = self.password.matches(self.character).count() as u32;
         return self.min <= counts && counts <= self.max;
+    }
+
+    fn is_valid_new_policy(&self) -> bool {
+        let first_position_char = self.password.chars().nth(self.min as usize - 1).unwrap();
+        let second_position_char = self.password.chars().nth(self.max as usize - 1).unwrap();
+
+        let is_same = first_position_char == second_position_char;
+
+        if first_position_char == self.character && !is_same {
+            return true;
+        }
+
+        if second_position_char == self.character && !is_same {
+            return true;
+        }
+
+        return false;
     }
 }
 
@@ -54,7 +75,19 @@ fn part_1(entries: Vec<PasswordPolicy>) -> i32 {
     let mut num_of_valid_passwords = 0;
 
     for entry in entries {
-        if entry.is_valid() {
+        if entry.is_valid_old_policy() {
+            num_of_valid_passwords += 1;
+        }
+    }
+
+    return num_of_valid_passwords;
+}
+
+fn part_2(entries: Vec<PasswordPolicy>) -> i32 {
+    let mut num_of_valid_passwords = 0;
+
+    for entry in entries {
+        if entry.is_valid_new_policy() {
             num_of_valid_passwords += 1;
         }
     }
@@ -73,5 +106,6 @@ mod tests {
         let puzzle_inputs = parse_input(input_string);
 
         assert_eq!(part_1(puzzle_inputs.clone()), 625);
+        assert_eq!(part_2(puzzle_inputs), 391);        
     }
 }
