7c4f91ac817f829d8d17029002f5e2d696e25c50
Alberto Leal <mail4alberto@gmail.com>
2023-01-03 02:03:29 +0900

    2022: day 9 part 2

--- 2022/day-09-2022/src/main.rs
+++ 2022/day-09-2022/src/main.rs
@@ -43,14 +43,12 @@ impl Transitions for Coordinate {
 
 #[derive(Debug, Clone)]
 struct Rope {
-    head: Coordinate,
-    tail: Coordinate,
-
+    knots: Vec<Coordinate>,
     tail_visits: HashSet<Coordinate>,
 }
 
 impl Rope {
-    fn new() -> Self {
+    fn new(num_of_body_knots: u32) -> Self {
         let starting_point: Coordinate = (0, 0);
         let tail_visits = {
             let mut tail_visits = HashSet::new();
@@ -58,92 +56,143 @@ impl Rope {
             tail_visits
         };
 
-        Rope {
-            head: starting_point,
-            tail: starting_point,
+        let mut knots: Vec<Coordinate> = vec![(0, 0), (0, 0)];
 
-            tail_visits,
+        for _ in 1..=num_of_body_knots {
+            knots.push(starting_point);
         }
+
+        Rope { knots, tail_visits }
     }
 
     fn parse_move(&mut self, parsed_move_step: Move) {
         match parsed_move_step {
             Move::Up(direction_length) => {
                 for _ in 1..=direction_length {
-                    self.head = self.head.up();
-                    self.update_tail_position();
+                    self.knots[0] = self.knots[0].up();
+                    self.update_body_positions();
                 }
             }
             Move::Down(direction_length) => {
                 for _ in 1..=direction_length {
-                    self.head = self.head.down();
-                    self.update_tail_position();
+                    self.knots[0] = self.knots[0].down();
+                    self.update_body_positions();
                 }
             }
             Move::Left(direction_length) => {
                 for _ in 1..=direction_length {
-                    self.head = self.head.left();
-                    self.update_tail_position();
+                    self.knots[0] = self.knots[0].left();
+                    self.update_body_positions();
                 }
             }
             Move::Right(direction_length) => {
                 for _ in 1..=direction_length {
-                    self.head = self.head.right();
-                    self.update_tail_position();
+                    self.knots[0] = self.knots[0].right();
+                    self.update_body_positions();
                 }
             }
         }
     }
 
-    fn update_tail_position(&mut self) {
-        let (head_x, head_y) = self.head;
-        let (tail_x, tail_y) = self.tail;
+    fn update_body_positions(&mut self) {
+        let num_of_knots = self.knots.len();
+        for index in 1..=(num_of_knots - 1) {
+            self.update_knot_position(index);
+        }
+    }
+
+    fn update_knot_position(&mut self, index_of_knot: usize) {
+        assert!(index_of_knot > 0);
+
+        let is_last_knot = index_of_knot == (self.knots.len() - 1);
+
+        let prev_knot = self.knots[index_of_knot - 1];
+        let current_knot = &mut self.knots[index_of_knot];
+
+        let (prev_knot_x, prev_knot_y) = prev_knot;
+        let (current_knot_x, current_knot_y) = *current_knot;
 
         // Tail must always be within two steps directly up, down, left, or right away from head.
-        let x_distance = (head_x - tail_x).abs();
-        let y_distance = (head_y - tail_y).abs();
+        let x_distance = (prev_knot_x - current_knot_x).abs();
+        let y_distance = (prev_knot_y - current_knot_y).abs();
         assert!(x_distance <= 2);
         assert!(y_distance <= 2);
-        let same_row = head_y == tail_y;
-        let same_col = head_x == tail_x;
+        let same_row = prev_knot_y == current_knot_y;
+        let same_col = prev_knot_x == current_knot_x;
         let head_and_tail_touching = x_distance == 1 && y_distance == 1;
 
         // If the head is ever two steps directly up, down, left, or right from the tail, the tail must also move one
         // step in that direction so it remains close enough
         if x_distance == 2 && same_row {
-            if head_x > tail_x {
-                self.tail = self.tail.right();
+            if prev_knot_x > current_knot_x {
+                *current_knot = current_knot.right();
             } else {
-                self.tail = self.tail.left();
+                *current_knot = current_knot.left();
             }
         } else if y_distance == 2 && same_col {
-            if head_y > tail_y {
-                self.tail = self.tail.up();
+            if prev_knot_y > current_knot_y {
+                *current_knot = current_knot.up();
             } else {
-                self.tail = self.tail.down();
+                *current_knot = current_knot.down();
             }
 
         // if the head and tail aren't touching and aren't in the same row or column, the tail always moves one step
         // diagonally to keep up
         } else if !head_and_tail_touching && !same_row && !same_col {
-            if head_x > tail_x {
-                self.tail = self.tail.right();
+            if prev_knot_x > current_knot_x {
+                *current_knot = current_knot.right();
             } else {
-                self.tail = self.tail.left();
+                *current_knot = current_knot.left();
             }
 
-            if head_y > tail_y {
-                self.tail = self.tail.up();
+            if prev_knot_y > current_knot_y {
+                *current_knot = current_knot.up();
             } else {
-                self.tail = self.tail.down();
+                *current_knot = current_knot.down();
             }
         }
-        self.tail_visits.insert(self.tail);
+
+        if is_last_knot {
+            self.tail_visits.insert(*current_knot);
+        }
     }
 }
 
 fn part_1(input_string: String) -> usize {
-    let mut rope = Rope::new();
+    let mut rope = Rope::new(0);
+
+    for line in input_string.trim().lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+
+        let parsed_move_step = {
+            let parsed: Vec<&str> = line.split_whitespace().collect();
+            assert_eq!(parsed.len(), 2);
+            let direction = parsed[0];
+            let direction_length: u32 = parsed[1].parse().unwrap();
+            assert!(direction_length > 0);
+
+            match direction {
+                "U" => Move::Up(direction_length),
+                "D" => Move::Down(direction_length),
+                "L" => Move::Left(direction_length),
+                "R" => Move::Right(direction_length),
+                _ => {
+                    unreachable!();
+                }
+            }
+        };
+
+        rope.parse_move(parsed_move_step);
+    }
+
+    rope.tail_visits.len()
+}
+
+fn part_2(input_string: String) -> usize {
+    let mut rope = Rope::new(8);
 
     for line in input_string.trim().lines() {
         let line = line.trim();
@@ -178,10 +227,13 @@ fn part_1(input_string: String) -> usize {
 fn main() {
     let input_string = include_str!("input.txt").to_string();
 
-    let tail_visits = part_1(input_string);
-
+    let tail_visits = part_1(input_string.clone());
     println!("Part 1: {}", tail_visits);
     assert_eq!(tail_visits, 6357);
+
+    let tail_visits = part_2(input_string);
+    println!("Part 2: {}", tail_visits);
+    assert_eq!(tail_visits, 2627);
 }
 
 #[cfg(test)]
@@ -189,7 +241,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_example() {
+    fn test_part_1() {
         let input_string = r###"
 R 4
 U 4
@@ -203,6 +255,36 @@ R 2
         .to_string();
 
         assert_eq!(part_1(input_string.to_string()), 13);
-        // assert_eq!(part_2(input_string.to_string()), 8);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let input_string = r###"
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
+"###
+        .to_string();
+
+        assert_eq!(part_2(input_string.to_string()), 1);
+
+        let input_string = r###"
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"###
+        .to_string();
+
+        assert_eq!(part_2(input_string.to_string()), 36);
     }
 }
