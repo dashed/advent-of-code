d63e267de505d3f980ad4eb8791055b87a059b99
Alberto Leal <mail4alberto@gmail.com>
2023-12-03 22:34:47 -0500

    2023: day 2

--- 2023/day-01-2023/src/main.rs
+++ 2023/day-01-2023/src/main.rs
@@ -4,6 +4,12 @@ fn main() {
     // Part 1
 
     println!("Part 1: {}", part_1(input_string));
+
+    // Part 2
+
+    let result = part_2(input_string);
+    println!("Part 2: {}", result);
+    assert_eq!(result, 54019);
 }
 
 fn part_1(input_string: &str) -> i32 {
@@ -28,6 +34,72 @@ fn part_1(input_string: &str) -> i32 {
     numbers.into_iter().sum()
 }
 
+fn parse_number_word(chars: &[char]) -> Option<(i32, i32)> {
+    let number_words = vec![
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    for (index, number_word) in number_words.into_iter().enumerate() {
+        let number_word_length = number_word.len();
+        let number_word = number_word.chars().collect::<Vec<char>>();
+        if chars.starts_with(&number_word) {
+            let number = index + 1;
+            return Some((number as i32, number_word_length as i32));
+        }
+    }
+
+    None
+}
+
+fn part_2(input_string: &str) -> i32 {
+    let inputs: Vec<&str> = input_string.trim().lines().collect();
+    let mut numbers: Vec<i32> = vec![];
+
+    for input in inputs {
+        let mut chars: Vec<char> = input.chars().collect();
+
+        let mut digits: Vec<i32> = vec![];
+
+        loop {
+            if chars.is_empty() {
+                break;
+            }
+
+            let first_char = chars.first().unwrap();
+            if first_char.is_ascii_digit() {
+                let number = first_char.to_digit(10).unwrap() as i32;
+                digits.push(number);
+                chars.remove(0);
+                continue;
+            }
+
+            let result = parse_number_word(&chars);
+            if result.is_none() {
+                chars.remove(0);
+                continue;
+            }
+
+            let (number, number_word_length) = result.unwrap();
+            digits.push(number);
+            // chars.drain(0..number_word_length as usize);
+            // Preserve the last character of the number word to cover cases such as eighthree and sevenine.
+            // The instructions doesn't make this clear.
+            chars.drain(0..(number_word_length - 1) as usize);
+        }
+
+        if digits.is_empty() {
+            continue;
+        }
+
+        let first_calibration_value = digits.first().unwrap();
+        let second_calibration_value = digits.last().unwrap();
+        let calibration_value = format!("{}{}", first_calibration_value, second_calibration_value);
+        let number = calibration_value.parse::<i32>().unwrap();
+        numbers.push(number);
+    }
+
+    numbers.into_iter().sum()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -42,5 +114,24 @@ treb7uchet
 "###;
 
         assert_eq!(part_1(input_string.clone()), 142);
+
+        let input_string = r###"
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+"###;
+
+        assert_eq!(part_2(input_string.clone()), 281);
+
+        let input_string = r###"
+eighthree
+sevenine
+"###;
+
+        assert_eq!(part_2(input_string.clone()), 83 + 79);
     }
 }
