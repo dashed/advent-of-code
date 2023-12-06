463bfe52e42fdacc27fc2514552ddf51a81c9017
Alberto Leal <mail4alberto@gmail.com>
2023-12-06 03:52:01 -0500

    init 2023 day 4

---
+++ 2023/day-04-2023/src/main.rs
@@ -0,0 +1,16 @@
+// https://adventofcode.com/2023/day/4
+
+fn part_1(input_string: &str) -> i32 {
+
+    0
+}
+
+fn main() {
+    let input_string = include_str!("input.txt");
+
+    // Part 1
+
+    let answer = part_1(input_string);
+    println!("Part 1: {}", answer);
+    assert_eq!(answer, 539637);
+}
